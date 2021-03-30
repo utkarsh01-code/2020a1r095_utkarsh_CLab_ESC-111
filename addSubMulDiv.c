12311ddf9a25b01601b8d2c2492ee07e86adb3aa
utkarsh01-code <75720483+utkarsh01-code@users.noreply.github.com>
2021-03-30 17:17:33 +0530

    Add files via upload

---
+++ addSubMulDiv.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+int main(){
+ int a,b,add,mul,div,sub;
+ printf("value of no. a and b\n");
+ scanf("%d%d",&a,&b);
+ add=a+b;
+ sub=a-b;
+ mul=a*b;
+ div=a/b;
+ printf("%d\n",add);
+ printf("%d\n",sub);
+ printf("%d\n",mul);
+ printf("%d\n",div);
+ return 0;
+}
+
+    
