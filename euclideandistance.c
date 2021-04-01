96c94eada3a00f90e9893fe927b9041e6e29c148
utkarsh01-code <75720483+utkarsh01-code@users.noreply.github.com>
2021-04-01 09:05:07 +0530

    Add files via upload

---
+++ euclideandistance.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float x1, y1, x2, y2, distance;
+	printf("Input x1: ");
+	scanf("%f", &x1);
+	printf("Input y1: ");
+	scanf("%f", &y1);
+    printf("Input x2: ");
+	scanf("%f", &x2);
+	printf("Input y2: ");
+	scanf("%f", &y2);
+	distance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+	printf("Distance between the said points: %f", sqrt(distance));
+	printf("\n");
+	return 0;
+}
