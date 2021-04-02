af97b097c2010afb1b9c946cc6d54b34e9eb52e4
utkarsh01-code <75720483+utkarsh01-code@users.noreply.github.com>
2021-04-02 10:58:45 +0530

    Add files via upload

---
+++ maxOfNnos.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int i,num,n,large=0;
+
+  printf("How many numbers: ");
+  scanf("%d",&n);
+  
+  for(i=0; i<n; i++)
+  {
+    printf("\nEnter number %d: ",i+1);
+    scanf("%d",&num);
+    if(num>large)
+    large=num;
+  }
+
+  printf("\n\nThe Largest Number is %d",large);
+  
+  return 0;
+}
