40d5cc94c1d7e79109a8c3c9f050a1b7974cbaeb
utkarsh01-code <75720483+utkarsh01-code@users.noreply.github.com>
2021-04-02 11:07:38 +0530

    Add files via upload

---
+++ primeNos.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{
+      int num,i=1,c=0;
+      printf("/*To Check Number Prime or Not*/\n\nEnter Number : ");
+      scanf("%d",&num);
+      while(i<=num)
+      {
+            if(num%i==0)
+            c++;
+            i++;
+      }
+      if(c==2)
+            printf("\n%d is Prime Number",num);
+      else
+            printf("\n%d is Not Prime Number",num);
+      return 0;
+}
